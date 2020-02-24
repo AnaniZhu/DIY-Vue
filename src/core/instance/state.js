4a02301c2337ad6978782eb73a57875c0076a449
zhuwenhan <zhuwenhan@dxy.cn>
2020-02-24 20:57:24 +0800

    docs: 修改 computed 实现思路的注释

--- src/core/instance/state.js
+++ src/core/instance/state.js
@@ -74,15 +74,48 @@ function initComputed (vm) {
       const getter = isFn ? val : val.get
       /**
      * 如果不考虑 computed 的缓存特性，都不需要定义为 watcher, 只需要把 computed 的每一个 key 代理到 vm 上，定义 getter 就好了
-     * 为了实现缓存的特性，就加入了很大的改动, 比如:
+     * 为了实现缓存的特性，就产生了很大的改动, 比如:
      * - 为了检测到所依赖的值的变化，来判断缓存的值是否失效，就要引入 watcher
-     * - 引入了 watcher 之后，需要自定义 getter 来判断缓存有效性，再决定走用户定义的 get 还是缓存
-     * - 再考虑到 computed 互相依赖的情况，会形成嵌套 watcher。
+     * - 引入了 watcher 之后，需要自定义 getter 来判断缓存有效性，再决定走用户定义的 getter 还是缓存
+     * - 再考虑到存在 computed 互相依赖的情况，会形成嵌套 watcher。
      * - watcher getter 中的依赖更改后，为了正确触发 watcher update，就要引入栈(Dep 里的 targetStack)的概念，用来存放这些嵌套 watchers
-     * - 还存在一个依赖属性被多个 computed 所依赖，这些 computed 就被 renderWatcher 所依赖
-     * - 当依赖属性发生改变后，通知 computed(lazy watcher) update, 可能会重复调用 renderWatcher。
-     * - 因为解决上述多种原因，所以 watcher 上也要维护 deps, 由所依赖项统一收集 watchers 并 update，加上去重，就不会重复更新
+     * - 如何保证嵌套 watchers 被 dep 正确收集，且不能产生重复更新?。
+     * - 因为解决上述多种问题，决定 watcher 上也维护一份 deps,  dep 执行 depend 时同时将自身(dep)存入 watcher 中，
+     * - 嵌套 watcher 时，子级 watcher 的 deps 把该父级 watcher 也收集进去，就可以解决上述问题。
      * - 而当 watchers 要维护 deps 后，又得处理去重以及清理操作...
+     *
+     *
+     * 在未引入 scheduler 的概念之前，面对嵌套 watcher 情况，为了解决的 computed(lazy watcher) 正确更新的问题，我一开始尝试了以下两个思路:
+     * 思路一:
+     * - dep 只收集当前的 Dep.target, 每个 watcher 维护一个自己的 dep, 用来存放上一层嵌套 watcher，
+     * - 然后被依赖的响应式属性发生改变，通知该 watcher, watcher update 后进而通过自身的 dep 再通知上一个嵌套的 watcher, 以此类推。
+     * - 结果存在问题，看如下例子:
+     * - data: {num: 1}, computed: { count (){return num}},  render() {return this.num + this.count}
+     * - 当发生 render 时，Dep.target 是 renderWatcher, 此时按顺序:
+     * - 先触发 num 的 get，num 的 dep 执行 depend 收集 renderWatcher
+     * - 然后触发 count 的 get，count 的 dep 收集 renderWatcher, 同时将 Dep.target 赋值为 countWatcher
+     * - count get 又会触发 num 的 get，num 的 dep 执行 depend 收集 Dep.target (countWatcher)
+     * - 此时 num dep: [renderWatcher, countWatcher], count dep: [renderWatcher]
+     * - 当 num 重新赋值, dep notify 对 watchers  排序更新，先触发 countWatcher 的 update，将 countWatcher dirty 设置为 true
+     * - 同时触发 countWatcher 上 dep 收集到的 renderWatcher 的 update，而此时 num dep 里还有一个 renderWatcher, 导致重复更新
+     * - 综上所述，思路一失败。
+     * 思路二:
+     * - 引入栈的概念，将嵌套 watchers 按顺序入栈，触发响应式属性 dep depend 时，将栈内 watcher 全部收集。
+     * - 结果依旧存在问题，看如下例子:
+     * - data: {num: 1}, computed: { count (){return num * 2}, text () {return count + '次'}}, render() {return this.num + this.count + this.text}
+     * - 当发生 render 时，renderWatcher 入栈，先触发 num get, num 的 dep 执行 depend, 此时 num dep 的 subs 为 [renderWatcher]
+     * - 然后触发 count get，countWatcher 入栈，count get 触发 num get， num dep 开始 depend 收集栈内 watchers: [renderWatcher, countWatcher]
+     * - 去重过滤后，num 的 dep subs 为: [renderWatcher, countWatcher]
+     * - count get 完成，同时将 countWatcher 的 dirty 属性设为 false，countWatcher 出栈
+     * - 再然后触发 text get, textWatcher 入栈，而 text 依赖 count，触发 count 的 get，
+     * - 但是 countWatcher 的 dirty 属性为 false，会直接走缓存，导致就不会执行用户定义的 count getter, 自然不会触发 num get
+     * - 此时导致依赖没有正确收集到，num 理应要收集到 textWatcher
+     * - 所以思路二也失败
+     *
+     * Vue 的官方实现就相当于是上面两种思路结合的变种。
+     * 响应式属性是唯一的依赖来源，watchers 通过 deps 属性当做响应式属性的载体，
+     * 用栈结构来处理嵌套 watchers 的关系，就像一个个桥梁连接到一起，每个桥梁上都有载体存放这对应的依赖，
+     * 这样依赖(响应式属性)就可以正确的访问到这座桥上的每一个 watcher
      */
       vm._computedWatchers[key] = new Watcher(vm, getter, () => {
       // vm[key] = val.get()
